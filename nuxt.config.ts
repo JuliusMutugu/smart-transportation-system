ace254239e5b64b860c18308654faf7a041db9ad
Julius_mutugu <ndegwajulius239@gmail.com>
2025-04-20 14:14:03 +0300

    removed unncessary modlues

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,13 +4,7 @@ export default defineNuxtConfig({
   devtools: { enabled: true },
 
   modules: [
-    '@nuxt/content',
-    '@nuxt/eslint',
-    '@nuxt/fonts',
-    '@nuxt/icon',
-    '@nuxt/image',
-    '@nuxt/scripts',
-    '@nuxt/test-utils',
+
     '@nuxt/ui'
   ]
 })
